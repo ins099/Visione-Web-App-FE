9880f98e1248ff2ddb10a2e724589d3993753d58
neha <n.nehakhan333@gmail.com>
2022-04-29 23:36:11 +0500

    feat:Customer data added

--- src/components/customer/Customer.jsx
+++ src/components/customer/Customer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Customer.css";
+import { CustomerData } from "./CustomerData";
 
 function Customer() {
   return (
@@ -10,20 +11,26 @@ function Customer() {
           Amazing layouts for your stunning website
         </h6>
       </div>
-      <div className="customer__container__content__section">
-        <blockquote className="customer__container__content__section__blockquote">
-          Fingerstache microdosing kickstarter porro quisquam est, qui Dolorem
-          ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non
-          numquam eius modi tempora incidunt ut Labore.
-        </blockquote>
-        <div className="customer__container__content__section__testimonials__avatar">
-            <img src="https://argenta.clbthemes.com/demo_01/wp-content/uploads/2016/11/arg_demo_avatar4-min.png"/>
-        </div>
-        <div className="customer__container__content__section__testimonials__content">
-            <h4 className="customer__container__content__section__testimonials__content__title">- Saga Norén</h4>
-            <p className="customer__container__content__section__testimonials__content__subtitle">Web Designer</p>
-        </div>
-      </div>
+      {CustomerData.map((x) => {
+        return (
+          <div className="customer__container__content__section">
+            <blockquote className="customer__container__content__section__blockquote">
+              {x.desc}
+            </blockquote>
+            <div className="customer__container__content__section__testimonials__avatar">
+              <img src={x.image} />
+            </div>
+            <div className="customer__container__content__section__testimonials__content">
+              <h4 className="customer__container__content__section__testimonials__content__title">
+                {x.title}
+              </h4>
+              <p className="customer__container__content__section__testimonials__content__subtitle">
+                {x.subtitle}
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/customer/CustomerData.jsx
@@ -0,0 +1,23 @@
+export const CustomerData = [
+  {
+    image:
+      "https://argenta.clbthemes.com/demo_01/wp-content/uploads/2016/11/arg_demo_avatar4-min.png",
+    title: "- Saga Norén",
+    subtitle: "Web Designer",
+    desc: "Fingerstache microdosing kickstarter porro quisquam est, qui Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut Labore.",
+  },
+  {
+    image:
+      "https://argenta.clbthemes.com/demo_01/wp-content/uploads/2016/11/arg_demo_avatar3-min.png",
+    title: "- John Storm",
+    subtitle: "UI/UX Designer",
+    desc: "Neque porro quisquam est, qui Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut Labore.",
+  },
+  {
+    image:
+      "https://argenta.clbthemes.com/demo_01/wp-content/uploads/2016/11/arg_demo_avatar2-min.png",
+    title: "- Bendigt Ferguson",
+    subtitle: "Product manager",
+    desc: "Neque porro quisquam est, qui Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut Labore.",
+  },
+];
